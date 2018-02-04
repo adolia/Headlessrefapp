889e9cc0ebacf1f044f8c3b8b0ee3ff87e47b789
Alex Dolia <aleksey.dolia@gmail.com>
2018-02-05 00:39:16 +0200

    Fix chrome launcher issue

--- chrome-profiler/data_profiler.js
+++ chrome-profiler/data_profiler.js
@@ -7,7 +7,7 @@ const sleep = n => new Promise(resolve => setTimeout(resolve, n));
 const url = 'http://localhost:9000';
 
 (async function() {
-  const chrome = await chromelauncher.launch({port: 9222});
+  const chrome = await chromeLauncher.launch({port: 9222});
   const client = await cdp();
 
     try {
